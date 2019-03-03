a1bb223d2e622ca1d8a1af620c2eb15d7601de03
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-03-02 15:32:37 -1000

    Timing Select Statements Completed

--- indexing.sql
+++ indexing.sql
@@ -3,6 +3,7 @@ DROP USER IF EXISTS indexed_cars_user;
 CREATE USER indexed_cars_user;
 CREATE DATABASE indexed_cars OWNER indexed_cars_user;
 \c indexed_cars;
+\timing
 \i scripts/car_models.sql;
 \i scripts/car_model_data.sql;
 \i scripts/car_model_data.sql;
@@ -16,3 +17,8 @@ CREATE DATABASE indexed_cars OWNER indexed_cars_user;
 \i scripts/car_model_data.sql;
 
 SELECT count(*) FROM car_models;
+SELECT DISTINCT make_title FROM car_models WHERE make_code = 'LAM';
+SELECT DISTINCT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R';
+SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code ='LAM';
+SELECT count(*) FROM car_models WHERE year <= 2015 AND year >= 2010;
+SELECT count(*) FROM car_models WHERE year = 2010;
